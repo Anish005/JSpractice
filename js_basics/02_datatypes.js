21b1876250660a86abfaf645ac79102c4f1054d3
Anish Panda <anishpanda31@gmail.com>
2023-11-20 21:01:43 +0530

    datatypes

---
+++ js_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3 + 3) we are using node js not browser
+
+// write code for good user readablility
+//tc39 or mdn for documentation
+
+let name = "anish"
+let age = 34
+let isLogged = true
+let state = null
+//number => 2 to 53
+// bigint for big wesbites
+//string => ""
+//boolean = true/false
+//null => standalone value
+//undefined =>
+
+//symbol
+
+
+//object
+console.log(typeof "anish");//string
+console.log(typeof  undefined); //undefined
+console.log(typeof null);//object
