1fb125d7dcc4f5b9c71a5195059ba3a111734b90
Anish Panda <anishpanda31@gmail.com>
2023-11-20 20:20:07 +0530

    variables

---
+++ js_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 2332423;
+let accountEmail = "anishhey@gmail.com"; 
+var accountPasswd = "123434";
+accountCity = "Jaipur"; //possiblity
+
+let accountState
+
+//accountId = 2;
+accountEmail = "rerejic.com";
+accountCity = "Delhi";
+accountPasswd = 232323;
+
+console.log(accountId);
+/*prefer not to use var because of issue in block scope and functional scope */
+console.table([accountEmail,accountId,accountPasswd,accountCity,accountState]);
