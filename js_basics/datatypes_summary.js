4ac2aaf132c269836b163e70dd34f36814a7fa2f
Anish Panda <anishpanda31@gmail.com>
2023-11-21 15:18:29 +0530

    datatype_summary

--- js_basics/datatypes_summary.js
+++ js_basics/datatypes_summary.js
@@ -1,1 +1,37 @@
-//summarizing the datatypes
+//summarizing the datatypes-- primitive and non - primitive
+//Primitive (call by value) 
+//7 types-- String,Number,Boolean,null,undefined,Symbol(tomakethevalueunique),BigInt
+const score = 100
+const scoreValue = 100.304
+
+const isLogged = false
+const outsideTemp = null //typeof result ---> object
+let userEmail;//typeof ---> undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);//false
+
+const bigNumber = 32423423424234423423n
+
+
+//Non-primitive(Reference types)
+
+//Arrays,Objects,Functions
+
+const heroes = ['ironman','captain','doga']
+let Obj ={
+    name:"hitesh",
+    age: 25,
+}
+
+const myFunction = function (){ //typeof --> function(object function)
+    console.log("ANish");
+
+}
+
+
+
+
+
