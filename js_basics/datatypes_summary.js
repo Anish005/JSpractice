4342c305676f2e4d3e36776cdab847c568b92775
Anish Panda <anishpanda31@gmail.com>
2023-11-21 20:26:19 +0530

    datatype_summary

--- js_basics/datatypes_summary.js
+++ js_basics/datatypes_summary.js
@@ -31,7 +31,25 @@ const myFunction = function (){ //typeof --> function(object function)
 
 }
 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//memory ---- stack and heap
+//stack - primitive and heap - Non - primitive
 
+let myname = "anish"
+let anothername = "manish"
 
+console.log(myname);
+console.log(anothername);
+
+let userOne = {
+    email:"anishapnda@gmail.com",
+    upi:"dhbdhs@ybl"
+}
+let userTwo = userOne
+
+userTwo.email = "anishapnfda@amazon.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);//they point to the same reference so it changes the value in both
 
 
