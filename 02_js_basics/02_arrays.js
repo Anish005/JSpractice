bcf2b682cc94f849cb708a48c39c9b5d0a978b8e
Anish Panda <anishpanda31@gmail.com>
2023-11-23 17:20:09 +0530

    arrays2

---
+++ 02_js_basics/02_arrays.js
@@ -0,0 +1,34 @@
+const marvel = ["thor","hulk" ,"spidey"]
+const dc = ["superman","flash","batman"]
+
+// marvel.push(dc)//enters arrays as a data on the same array kindof listoflists
+// console.log(marvel[3][1]);
+// console.log(marvel);
+
+
+// const allheroes = marvel.concat(dc) //concat --> requires another array to return the merged arrays
+// console.log(allheroes);
+
+//spread operator
+
+const all_new_heroes = [...marvel,...dc]
+console.log(all_new_heroes);
+
+const otherarray = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const real_another_array = otherarray.flat(Infinity)//flatens the arrays 
+console.log(real_another_array);
+
+
+
+console.log(Array.isArray("Anish"))
+console.log(Array.from("Anish"))//converts into array this string
+
+console.log(Array.from({name:"Anish"}));//output-->[] u have to specify for  keys/names
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
+
+
+
