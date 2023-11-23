4514e2f67f30d8c89b2b369abad3c8367582b70a
Anish Panda <anishpanda31@gmail.com>
2023-11-23 21:26:18 +0530

    functions

--- 03_js_basics/01_functions.js
+++ 03_js_basics/01_functions.js
@@ -40,4 +40,41 @@ function loginUsermsg(username) {// u can pass in the parameter for the default
 
 //console.log(loginUsermsg("anish"))
 
-console.log(loginUsermsg());//result shows undefined just logged in 
+//console.log(loginUsermsg());//result shows undefined just logged in 
+
+function calcCarPrice(...n1) {//passing multiple values
+    return n1
+}
+console.log(calcCarPrice(2));//outputs in an array
+console.log(calcCarPrice(2,2,43,4,4,4111,1));
+//to add more numbers of parameters ... iscalled in this scenario rest operator other case separator
+
+function calcCar(val1,val2,...num1) {
+    return num1
+}
+console.log(calcCar(1,2,34,4,42,212));//removes the val1,val2 and returns other values in an array
+
+
+const user ={
+    username:"anish",
+    price:199
+}
+
+
+function handleObj(anyObject) {
+    console.log(`Username is ${anyObject.username} and the price is ${anyObject.price}`);
+}
+//handleObj(user)
+handleObj({
+    username:"sam",
+    price:3223
+})
+
+const newArray = [200,44,444,3433]
+
+function returnSecond(getArray) {
+    return getArray[1]
+    
+}
+console.log(returnSecond(newArray));
+console.log(returnSecond([23,334,4545,454,345]));
