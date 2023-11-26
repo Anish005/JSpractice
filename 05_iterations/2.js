74c790ee87d318544c219f3c2aaff4a143e9887e
Anish Panda <anishpanda31@gmail.com>
2023-11-26 08:34:07 +0530

    loops

---
+++ 05_iterations/2.js
@@ -0,0 +1,31 @@
+// while (condition) {
+    
+// }
+let index = 0
+while (index <= 5) {
+ //   console.log(`value of index is ${index}`);
+    index += 2
+}
+
+myArray = ['flash','batman','superman']
+let arr = 0
+while (arr < myArray.length) {
+   // console.log(`value is ${myArray[arr]}`);
+    arr += 1
+}
+//do while loop
+// do {
+    
+// } while (condition);
+let score = 10
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <=10);//score 11 if sccore is 11
+
+
+
+
+
+
+
